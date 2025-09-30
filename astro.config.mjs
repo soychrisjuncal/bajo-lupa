0061c57d2c47da988b62f94dd74272f77fb22c21
soychrisjuncal <juncalcf@gmail.com>
2025-09-30 14:57:57 -0300

    🔧 Recrear deploy.yml con configuración correcta

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,5 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://soychrisjuncal.github.io',
   base: '/bajo-lupa',
