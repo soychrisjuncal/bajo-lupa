a8b12308bf6d824dec169e98e504b98de8f8690a
soychrisjuncal <juncalcf@gmail.com>
2025-09-30 15:27:02 -0300

    🗺️ Agregar sitemap para SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://soychrisjuncal.github.io',
   base: '/bajo-lupa',
+  integrations: [sitemap()],
 });
