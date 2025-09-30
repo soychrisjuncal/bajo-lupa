f7dd2b7c94e7b36da52555fde6fee942a599f193
soychrisjuncal <juncalcf@gmail.com>
2025-09-30 14:35:47 -0300

    🚀 Versión inicial del blog Bajo la Lupa

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://soychrisjuncal.github.io',
+  base: '/bajo-lupa',
+});
